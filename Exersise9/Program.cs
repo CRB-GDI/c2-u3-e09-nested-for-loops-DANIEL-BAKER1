a0beffc49bd08d1a150dd6a4c3d590b347c9d785
Daniel Baker <Daniel.Baker@s.capregboces.org>
2024-03-06 13:13:52 -0500

    Completed Exercise9 problem 2

--- Exersise9/Program.cs
+++ Exersise9/Program.cs
@@ -12,7 +12,7 @@
 
             //print rows going down
             Console.WriteLine();
-            for (int a =1; a <= height; a++)
+            for (int a = 1; a <= height; a++)
             {
                 //print content of the row to the right
                 for (int b = 1; b <= width; b++)
@@ -21,6 +21,22 @@
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("Enter the MAX amount of asterisks: ");
+            int width2 = int.Parse(Console.ReadLine());
+
+            
+
+            //printing MAX amount of asterisks
+            Console.WriteLine();
+            for (int a = 1; a <= width2; a++)
+            {
+                for (int b = 1; b <= a; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+           
         }
     }
 }
